c7af72e20673db6a8b87c61f644bf40802ad923f
Ali Can Batur <baturalican@hotmail.com>
2024-08-02 18:55:16 +0200

    feat: react learning add multiple jsx slots to components + some proper formatting for the jsx files

--- React/Learning/01-starting-project/01-starting-project/src/components/Section.jsx
+++ React/Learning/01-starting-project/01-starting-project/src/components/Section.jsx
@@ -1,6 +1,8 @@
-export default function Section({title, children, ...props}) {
-    return <section {...props}>
-        <h2>{title}</h2>
-        {children}
+export default function Section({ title, children, ...props }) {
+  return (
+    <section {...props}>
+      <h2>{title}</h2>
+      {children}
     </section>
+  );
 }
