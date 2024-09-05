ec503252fd35290b90f9af8a8cbe967add592c85
Ali Can Batur <baturalican@hotmail.com>
2024-09-06 00:11:39 +0200

    feat: react learning add edit button clickability functionality to game board area

--- React/Learning/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ React/Learning/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  // false is the initial state
+  // isEditing is the state variable
+  // setIsEditing is the function to change the isEditing value
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName =  <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required/>
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
