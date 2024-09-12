ee22cf99d68b105ae71a471bb92885be14f68c5a
Ali Can Batur <baturalican@hotmail.com>
2024-09-12 16:56:32 +0200

    feat: react learning add input field functionality when entering player names in tic-tac-toe game

--- React/Learning/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ React/Learning/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ intialName, symbol }) {
+  const [playerName, setPlayerName] = useState(intialName);
+
   // false is the initial state
   // isEditing is the state variable
   // setIsEditing is the function to change the isEditing value
@@ -13,19 +15,24 @@ export default function Player({ name, symbol }) {
     // Because if we do that, React schedules the update of the variable (isEditing)
     // It does not change it immediately!!!
     setIsEditing((editing) => !editing);
-  
   }
 
-  let playerName =  <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name}/>
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
